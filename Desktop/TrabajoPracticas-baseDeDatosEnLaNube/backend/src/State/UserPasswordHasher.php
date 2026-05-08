21f1b772481b01824d3516444bdb19a6f7b6433c
AnaIsabel Dev <anaisabel.mendoza@escuelaestech.es>
2026-05-08 12:57:03 +0200

    Solucionado el guardado en base de datos

--- Desktop/TrabajoPracticas-baseDeDatosEnLaNube/backend/src/State/UserPasswordHasher.php
+++ Desktop/TrabajoPracticas-baseDeDatosEnLaNube/backend/src/State/UserPasswordHasher.php
@@ -6,10 +6,14 @@ use ApiPlatform\Metadata\Operation;
 use ApiPlatform\State\ProcessorInterface;
 use App\Entity\User;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+// ¡ESTA LÍNEA ES NUEVA E IMPORTANTE! 👇
+use Symfony\Component\DependencyInjection\Attribute\Autowire;
 
 class UserPasswordHasher implements ProcessorInterface
 {
     public function __construct(
+        // ¡ESTA ETIQUETA ES LA MAGIA QUE GUARDA EN LA BASE DE DATOS! 👇
+        #[Autowire(service: 'api_platform.doctrine.orm.state.persist_processor')]
         private readonly ProcessorInterface $persistProcessor,
         private readonly UserPasswordHasherInterface $passwordHasher
     ) {}
@@ -29,6 +33,7 @@ class UserPasswordHasher implements ProcessorInterface
             }
         }
 
+        // Ahora sí, esta línea cogerá el guardador de Doctrine y escribirá en Aiven
         return $this->persistProcessor->process($data, $operation, $uriVariables, $context);
     }
 }
